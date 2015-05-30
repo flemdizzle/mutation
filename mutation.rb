36429f6ab3ff0c219628b08d22eb46fdabb5648b
Steven Fleming <sfleming@optoro.com>
2015-05-30 00:29:22 -0400

    started writing out the compare method

--- mutation.rb
+++ mutation.rb
@@ -13,14 +13,24 @@ class Mutation
   end
 
   def breed
-    10.times {offspring << specimen.name}
+    specimen[:generation] += 1
+    100.times {offspring << specimen.name}
   end
 
+  #mutate needs to do more than just add things
   def mutate
     offspring.each { |n| n << rand(36).to_s(36) if rand(5) == 0 }
   end
 
   def compare
+    offspring.each do |n|
+      return false if n === result
+
+    end
+    #compare length
+    #compare string letter values
+    #pick best fit or match
+    #return true if not match
 
   end
 
